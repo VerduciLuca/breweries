24a07c1a4fb914b55a76e19c331c42466c8ab7a3
VerduciLuca <135212650+VerduciLuca@users.noreply.github.com>
2023-07-17 09:05:24 +0200

    dasdasd

--- db-service.js
+++ db-service.js
@@ -1,7 +1,7 @@
 class DBService{
 
     static getBreweries (pageNumber){
-        const url = 'https://api.openbrewerydb.org/v1/breweries?per_page=10&page=' + pageNumber
+        const url = 'https://api.openbrewerydb.org/v1/breweries?per_page=14&page=' + pageNumber
 
         return fetch(url)
         .then(resp=> resp.json())
